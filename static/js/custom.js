5ca20ab55c46f4cabc0a7558c0f8e9d76b5ca990
Seita Uchimura <c201552k@gmail.com>
2019-02-06 00:13:02 -0800

    :watermelon: Add:added slick

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,55 +1,9 @@
 $(document).ready(function() {
-    $("#light-slider").lightSlider({
-        item: 3,
-        autoWidth: false,
-        slideMove: 1, // slidemove will be 1 if loop is true
-        slideMargin: 10,
-
-        addClass: '',
-        mode: "slide",
-        useCSS: true,
-        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
-        easing: 'linear', //'for jquery animation',////
-
-        speed: 800, //ms'
-        auto: true,
-        loop: true,
-        slideEndAnimation: true,
-        pause: 2500,
-        pauseOnHover: true,
-
-        keyPress: false,
-        controls: true,
-        prevHtml: '',
-        nextHtml: '',
-
-        rtl:false,
-        adaptiveHeight:false,
-
-        vertical:false,
-        verticalHeight:500,
-        vThumbWidth:100,
-
-        thumbItem:10,
-        pager: true,
-        gallery: false,
-        galleryMargin: 5,
-        thumbMargin: 5,
-        currentPagerPosition: 'middle',
-
-        enableTouch:true,
-        enableDrag:true,
-        freeMove:true,
-        swipeThreshold: 40,
-
-        responsive : [],
-
-        onBeforeStart: function (el) {},
-        onSliderLoad: function (el) {},
-        onBeforeSlide: function (el) {},
-        onAfterSlide: function (el) {},
-        onBeforeNextSlide: function (el) {},
-        onBeforePrevSlide: function (el) {}
+    $('.slick').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
     });
 });
 
